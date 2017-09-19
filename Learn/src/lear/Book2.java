9437bc4444826bc4eb4f540ca369945e418e2dce
H0m1e <32064435+H0m1e@users.noreply.github.com>
2017-09-20 02:16:08 +0800

    Book2

---
+++ Learn/src/lear/Book2.java
@@ -0,0 +1,22 @@
+package lear;
+
+public class Book2 {
+    private String title;
+    private double price;
+    String pub="天天精彩出版社";
+    public   Book2(String title,double price) {
+		this.title=title;
+		this.price=price;
+		
+    }
+	}
+    public class BookPress{
+    	
+    
+	public static void main(String[] args) {
+		
+
+	}
+
+}
+
